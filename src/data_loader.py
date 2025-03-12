dfb152990ed4e243308a72c87c088e75d528059d
Anna Ivina <anna.ivina@cern.ch>
2025-03-12 20:06:26 +0200

    Add new aug using Albumentations

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,4 +1,5 @@
-from src.utils import preprocess_image
+#from src.utils import preprocess_image - Old preprocessing image file. This has been substituted to Albumentations
+import albumentations as A
 import tensorflow_datasets as tfds
 import tensorflow as tf
 
@@ -23,10 +24,39 @@ class DataLoader:
 
         #Preprocess the data so the image will be resied and will be float32
        
-        train_data = train_data.map(preprocess_image, num_parallel_calls=tf.data.AUTOTUNE).batch(self.batch_size).prefetch(buffer_size=tf.data.AUTOTUNE)
-        test_data = test_data.map(preprocess_image, num_parallel_calls=tf.data.AUTOTUNE).batch(self.batch_size).prefetch(buffer_size=tf.data.AUTOTUNE)
+        train_data = train_data.map(self.preprocess_image, num_parallel_calls=tf.data.AUTOTUNE).batch(self.batch_size).prefetch(buffer_size=tf.data.AUTOTUNE)
+        test_data = test_data.map(self.preprocess_image, num_parallel_calls=tf.data.AUTOTUNE).batch(self.batch_size).prefetch(buffer_size=tf.data.AUTOTUNE)
 
         return train_data, test_data
+    
 
+    #Implementing new libriary for data augmentation 
+    @staticmethod
+    def apply_transform(image):
 
+        img_size = 224
+        transformations = A.Compose(
+            [
+                #Resise only the EfficientNet does its own rescaling to 1/255
+                A.Resize(img_size, img_size),
+                A.OneOf([A.HorizontalFlip(),
+                         A.VerticalFlip()], p=0.3), #percent of times it iwll happen 
+                A.ShiftScaleRotate(shift_limit=0.1, scale_limit=0.3, rotate_limit=30, p=0.3),
+                A.Sharpen(alpha=(0.2,0.3), lightness=(0.5,1), p=0.5)
+            ]
+        )
+
+        #image = image.numpy()
+        image = transformations(image=image)['image']
+        
+        return tf.cast(image, tf.float32)
+    
+    @staticmethod
+    def preprocess_image(image, label):
+        image = tf.numpy_function(func=DataLoader.apply_transform, inp=[image], Tout=tf.float32)
+        image.set_shape((224,224,3))
+        return image, label
+        
+    
+ 
     
